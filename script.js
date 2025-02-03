62652ce3b31448a228435ee167590f6337b750cb
zuriqi2000 <117549570+zuriqi2000@users.noreply.github.com>
2025-02-03 13:57:44 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function addToCart(productName) {
+  alert(`${productName} has been added to your cart!`);
+  // You can add more functionality here, like updating a cart counter or storing items in localStorage.
+}
